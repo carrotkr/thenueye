8fda499a7a7cd4d248135fac528d71afc1b2cea2
lucas-sung <lucas@kakaopage.com>
2021-08-10 01:31:21 +0900

    add one-hit-wonders

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Main } from '../main';
 import PostCard from '../main/PostCard';
 import { Post } from '../type';
 
-const POSTS_SAMPLE: Post[] = [
+const POSTS: Post[] = [
   {
     topic: { id: 'sports', name: 'Sports' },
     title: 'One-Hit Wonders in Sports',
@@ -12,20 +12,6 @@ const POSTS_SAMPLE: Post[] = [
     publishedOn: 'April 25th, 2020',
     img: 'https://pudding.cool/common/assets/thumbnails/640/2018_04_one-hit-wonders.jpg',
     id: 'one-hit-wonders',
-  },
-  {
-    topic: { id: 'technology', name: 'Technology' },
-    title: 'Lorem Ipsum Dolor Sit Amet Dolor Sit Amet',
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis
-  porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis
-  dui porta volutpat. In sit amet posuere magna..`,
-    authors: [
-      { id: 'david-grzyb', name: 'David Grzyb' },
-      { id: 'mark', name: 'Mark' },
-    ],
-    publishedOn: 'April 25th, 2020',
-    img: 'https://source.unsplash.com/collection/1346951/1000x500?sig=1',
-    id: 'lorem-ipsum',
   },
 ];
 
@@ -43,8 +29,8 @@ const Index = () => {
       ]}
     >
       <section className="flex flex-col items-center px-3">
-        {POSTS_SAMPLE.map((post) => (
-          <PostCard key={post.id} {...post} />
+        {POSTS.map((post, index) => (
+          <PostCard key={index} {...post} />
         ))}
       </section>
     </Main>
